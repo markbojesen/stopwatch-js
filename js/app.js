4add59c9f20e0734b3a542c379c9df07ba7c53aa
Mark Bojesen <bojesenmark@gmail.com>
2017-07-24 21:06:19 +0100

    added comments in app.js for clarity

--- js/app.js
+++ js/app.js
@@ -1,26 +1,34 @@
 
-
+// Variables
 const startButton = document.querySelector('[data-action="start"]');
 const stopButton  = document.querySelector('[data-action="stop"]');
 const resetButton = document.querySelector('[data-action="reset"]');
 const minutes     = document.querySelector('.minutes');
 const seconds     = document.querySelector('.seconds');
 let timerTime     = 00;
+let isRunning     = false;
 let interval;
 
 
-
+// Functions
 function startTimer() {
+    if (isRunning) return;
+    isRunning = true;
     interval = setInterval(incrementTimer, 1000);
 }
 
 function stopTimer() {
-    
+    if (!isRunning) return;
+    isRunning = false;
+    clearInterval(interval);
 }
 
 
 function resetTimer() {
-    
+    stopTimer();
+    timerTime = 0;
+    minutes.innerHTML = '00';
+    seconds.innerHTML = '00';
 }
 
 function incrementTimer() {
@@ -33,6 +41,7 @@ function incrementTimer() {
     seconds.innerText = pad(timerTime);
 }
 
+// Make sure timer start with 00
 function pad(number) {
     return (number < 10) ? '0' + number : number;
 }
@@ -44,7 +53,7 @@ function pad(number) {
 // }
 
 
-
+// Event listeners
 startButton.addEventListener('click', startTimer);
 stopButton.addEventListener('click', stopTimer);
 resetButton.addEventListener('click', resetTimer);
